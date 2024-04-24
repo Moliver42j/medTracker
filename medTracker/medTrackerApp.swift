0675b142e2894e43e7de0126869eb3cdc1918bd8
Matthew Oliver <matthewoliver@Matthews-Laptop.local>
2024-04-24 13:53:41 +0100

    init

---
+++ medTracker/medTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  medTrackerApp.swift
+//  medTracker
+//
+//  Created by Matthew Oliver on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct medTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
